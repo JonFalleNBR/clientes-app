d459508c74fd691b7ca39f817f13a4224db92124
Jonathan-Silva <91348218+JonFalleNBR@users.noreply.github.com>
2024-04-10 00:36:05 -0300

    ServicoPrestado - Estruturação Logica e a criação do template HTML com o formulario de cadastro de um novo servico

--- src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../../clientes/cliente';
+import { ClientesService } from '../../clientes.service';
+import { response } from 'express';
+import { ServicoPrestado } from '../servicoPrestado';
+
+
 
 @Component({
   selector: 'app-servico-prestado-form',
   templateUrl: './servico-prestado-form.component.html',
   styleUrl: './servico-prestado-form.component.css'
 })
-export class ServicoPrestadoFormComponent {
+export class ServicoPrestadoFormComponent implements OnInit{
+
+  clientes: Cliente[] = [];
+  servico: ServicoPrestado;
+
+
+  constructor(
+    private clienteService : ClientesService
+  ){ this.servico = new ServicoPrestado(); }
+
+  ngOnInit(): void {
+    this.clienteService
+    .getClientes()
+    .subscribe ( response => this.clientes = response) //criando a listagem de clientes, tal qual no clientesListaComponent
+  }
+
+
+  onSubmit(){
+    console.log(this.servico)
+  }
 
 }
---
+++ src/app/servico-prestado/servicoPrestado.ts
@@ -0,0 +1,9 @@
+export class ServicoPrestado{
+
+     descricao: string = ' ';
+     preco:string = ' ';
+     data: string  = ' ';
+     idCliente: number = 0;
+
+    // Essa classe será um espelho do DTO da API Spring, recebendo os mesmos campos que estão la
+}
