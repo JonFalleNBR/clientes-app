3ac37cba450ff902a9dcaf79a80a1d423ca775c0
Jonathan-Silva <91348218+JonFalleNBR@users.noreply.github.com>
2024-04-12 19:17:38 -0300

    Login - Finalização do template de login com adição dos botoes de cadastro e cancelar

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,16 +11,28 @@ export class LoginComponent {
     username: string | null = null;
     password: string | null = null;
     loginError: boolean = false;
+    cadastrando: boolean = false;
 
-
-    constructor(){
+    constructor(
+      private router: Router
+    ){
         
     }
 
     onSubmit(){
       //this.username = this.username.trim();
-
       console.log(`User: ${this.username}, Pass: ${this.password}`)
+
+      this.router.navigate(['/home'])
+    }
+ 
+    preparaCadastrar(event: { preventDefault: () => void; }){
+      event.preventDefault();
+      this.cadastrando = true;
+    }
+
+    cancelaCadastro(){
+      this.cadastrando = false;
     }
 
 }
