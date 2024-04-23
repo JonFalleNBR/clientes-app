7bcee8329f150bde8e16648e471ce6a34828719b
Jonathan-Silva <91348218+JonFalleNBR@users.noreply.github.com>
2024-04-23 17:31:02 -0300

    uso da JWT para melhorias no Guard  - permitindo acesso a home apenas por autenticação do token - implementação da logica com nova biblioteca '@auth0/angular-jwt'

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,4 +1,4 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree  } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router  } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
@@ -12,17 +12,23 @@ import { Routes } from '@angular/router';
 export class AuthGuard implements CanActivate{
 
   constructor(
-      private authService : AuthService
-      //private routes: Routes
+      private authService : AuthService,
+      private router: Router
   ){}
 
   canActivate(
       next: ActivatedRouteSnapshot,
       state: RouterStateSnapshot): boolean {
 
-        return this.authService.isAuthenticated();
-   // const authenticated =  this.authService.isAuthenticated(); 
+        //return this.authService.isAuthenticated();
+        const authenticated =  this.authService.isAuthenticated(); 
 
+        if(authenticated){
+          return true;
+        }else{
+          this.router.navigate(['/login'])
+          return false
+        }
 
   }
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -4,6 +4,7 @@ import { Usuario } from './login/usuario';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment'
 
+import { JwtHelperService} from '@auth0/angular-jwt'
 
 @Injectable({
   providedIn: 'root'
@@ -14,12 +15,22 @@ export class AuthService {
   apiUrl : string = environment.apiURLBase + '/api/usuarios'
   tokenUrl: string = environment.apiURLBase + environment.obterTokenUrl
   clientID: string = environment.clientId
-  clientSecret: string = environment.clientSecret
+  clientSecret: string = environment.clientSecret;
+  jwtHelper : JwtHelperService = new JwtHelperService();
 
   constructor(
     private http : HttpClient
   ) { }
 
+
+  obterToken(){
+    const tokenString = localStorage.getItem('access_token')
+    if(tokenString){
+      const token = JSON.parse(tokenString).access_token
+      return token;
+    }
+    return null;
+  }
 
   salvar(usuario: Usuario): Observable<any>{
     return this.http.post<Usuario>(`${this.apiUrl}`, usuario)
@@ -27,8 +38,17 @@ export class AuthService {
 
   
   isAuthenticated() : boolean {
-    return true;
+    const token = this.obterToken();
+    if(token){
+      const expirated = this.jwtHelper.isTokenExpired(token);
+      return !expirated;
+    }
+    return false;
+    /*logica importante pois verifica a autenticação via token para acessar a  home e demais urls da aplicação que exijam autenticação
+        verificar classes auth.guard.ts e app-routing-module
+    */
   }
+
 
   tentarLogar(username: string, password: string): Observable<any>{
         const params = new HttpParams()
